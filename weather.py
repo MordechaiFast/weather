576567df08ce0e4731b35ec6e27f65804c3140a3
Mordechai Fast <mordechai.fast@gmail.com>
2023-08-02 00:56:14 +0300

    Beginings of CLI

--- weather.py
+++ weather.py
@@ -1,7 +1,24 @@
+from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
 
 
-def _get_api_key():
+def parse_args() -> Namespace:
+    """Handle CLI arguments."""
+    parser = ArgumentParser(
+        description='Gets weather forcast and temperature for a given city.'
+    )
+    parser.add_argument(
+        'city', nargs='+', type=str, 
+        help='Name of the CITY to check for'
+    )
+    parser.add_argument(
+        '-F', action='store_true', 
+        help='Display temperatures in F units'
+    )
+    parser.add_argument('--version', action='version', version='0.0')
+    return parser.parse_args()
+
+def _get_api_key() -> str:
     """Return the api_key for openweather from the configuration file.
     
     Expects configuration file named 'secrets.ini'
@@ -10,3 +27,6 @@ def _get_api_key():
     config.read('secrets.ini')
     return config['openweather']['api_key']
 
+
+if __name__ == '__main__':
+    parse_args()
