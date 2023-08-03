7021872d9d907dd9a0d52a292daabf1eb8683617
Mordechai Fast <mordechai.fast@gmail.com>
2023-08-04 01:22:54 +0300

    Humidity, clouds, lat&lon

--- weather.py
+++ weather.py
@@ -1,7 +1,10 @@
 import json
 import sys
+import time
 from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
+from datetime import datetime
+from pprint import pp
 from urllib import error, parse, request
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -37,7 +40,7 @@ def _get_api_key() -> str:
 
 def build_weather_query(city_input: list[str], fahrenheit=False) -> str:
     """Builds the URL for an API request to OpenWeather's weather API."""
-    city_name = " ".join(city_input)
+    city_name = ' '.join(city_input)
     encoded_city_name = parse.quote_plus(city_name)
     units = 'imperial' if fahrenheit else 'metric'
     api_key = _get_api_key()
@@ -50,7 +53,7 @@ def get_weather_data(query_url: str) -> dict:
         with request.urlopen(query_url) as response:
             data = response.read()
     except error.HTTPError as http_error:
-        if http_error.code == 401:  # Unauthorized
+        if   http_error.code == 401:  # Unauthorized
             sys.exit("Access denied. Check API key.")
         elif http_error.code == 404:  # Not found
             sys.exit("No weather data for this city.")
@@ -65,20 +68,69 @@ def get_weather_data(query_url: str) -> dict:
 
 
 def display(weather_data: dict, fahrenheit=False) -> None:
-    """Displays the city name, current tempurature in either C or F, 
-    the min and max tempuratures, and the general weather description.
+    """Displays the city name, current temperature in either C or F, 
+    the general weather description, lattitude and longitude, humidity, 
+    cloud cover, the min and max temperatures, and the sunrise and 
+    sunset times.
     """
     city = f"{weather_data['name']}, {weather_data['sys']['country']}:"
     temp_current = weather_data['main']['temp']
     weather_description = weather_data['weather'][0]['description']
+    raw_lattitude = weather_data['coord']['lat']
+    raw_longitude = weather_data['coord']['lon']
+    longitude = long_str(raw_longitude)
+    lattitude = lat_str(raw_lattitude)
+    humidity = weather_data['main']['humidity']
+    clouds = weather_data['clouds']['all']
     temp_min = weather_data['main']['temp_min']
     temp_max = weather_data['main']['temp_max']
     deg = '°F' if fahrenheit else '°C'
+    sunrise_utc = weather_data['sys']['sunrise']
+    sunset_utc = weather_data['sys']['sunset']
+    timezone = weather_data['timezone']
+    sunrise = local_time(sunrise_utc, timezone)
+    sunset = local_time(sunset_utc, timezone)
+
     print(f"{city:^{PADDING}} {temp_current:.0f}{deg}", end=' ')
     print(weather_description.capitalize())
+    print(f"{lattitude + ' ' + longitude:^{PADDING}} {humidity}% humidity")
+    print(" "*PADDING, f"{clouds}% cloud cover")
     print(" "*PADDING, f"Low: {temp_min:.0f}{deg} High:{temp_max:.0f}{deg}")
+    print(f"Sunrise: {time12hr(sunrise)} Sunset: {time12hr(sunset)}")
 
 
+def long_str(longitude: float) -> str:
+    """Converts longitude as degree measure into degrees and minutes 
+    with direction.
+    """
+    minutes = longitude % 1 * 60
+    return f"{abs(longitude):.0f}°{minutes:02.0f}'{'E' if longitude > 0 else 'W'}"
+
+
+def lat_str(lattitude: float) -> str:
+    """Converts lattitude as degree measure into degrees and minutes 
+    with direction.
+    """
+    minutes = lattitude % 1 * 60
+    return f"{abs(lattitude):.0f}°{minutes:02.0f}'{'N' if lattitude > 0 else 'S'}"
+
+
+def local_time(utc_time: int, timezone_delta: int) -> int:
+    """Converts a UTC timestamp into a local timestamp given the timedelta of the local time"""
+    local_offset = time.timezone if not time.daylight else time.altzone
+    return utc_time + timezone_delta + local_offset
+
+
+def time12hr(timestamp: int) -> str:
+    """Convert a Unix timestamp into a 12 hour time of day."""
+    date_format = datetime.fromtimestamp(timestamp)
+    hour = date_format.hour
+    minute = date_format.minute
+    if hour > 12:
+        hour -= 12
+    if hour == 0:
+        hour = 12
+    return f"{hour}:{minute:02}"
 
 
 if __name__ == '__main__':
@@ -86,3 +138,4 @@ if __name__ == '__main__':
     query_url = build_weather_query(args.city, args.F)
     weather_data = get_weather_data(query_url)
     display(weather_data, args.F)
+    #pp(weather_data)
